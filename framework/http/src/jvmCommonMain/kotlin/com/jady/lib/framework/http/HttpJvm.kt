d193bb8ef6cf9774d141c0a654693f509cdcb32f
Jadyli <lipingfa@bilibili.com>
2023-11-13 17:41:59 +0800

    Rename HttpModule.

--- framework/http/src/jvmCommonMain/kotlin/com/jady/lib/framework/http/HttpJvm.kt
+++ framework/http/src/jvmCommonMain/kotlin/com/jady/lib/framework/http/HttpJvm.kt
@@ -7,7 +7,7 @@ package com.jady.lib.framework.http
 import com.jady.lib.framework.http.config.BaseHttpConfig
 import io.ktor.client.*
 import io.ktor.client.engine.*
-import io.ktor.client.engine.darwin.*
+import io.ktor.client.engine.okhttp.*
 import org.koin.mp.KoinPlatform
 
 /**
@@ -15,9 +15,9 @@ import org.koin.mp.KoinPlatform
  * @since 2023/09/08 17/02
  * email: 1257984872@qq.com
  */
-actual fun createClient(commonConfig: HttpClientConfig<out HttpClientEngineConfig>.(BaseHttpConfig<*>) -> Unit): HttpClient {
-    val httpConfig = KoinPlatform.getKoin().get<BaseIosHttpConfig>()
-    return HttpClient(Darwin) {
+internal actual fun createClient(commonConfig: HttpClientConfig<out HttpClientEngineConfig>.(BaseHttpConfig<*>) -> Unit): HttpClient {
+    val httpConfig = KoinPlatform.getKoin().get<BaseOkHttpHttpConfig>()
+    return HttpClient(OkHttp) {
         commonConfig(httpConfig)
         with(httpConfig) {
             config()
@@ -25,4 +25,4 @@ actual fun createClient(commonConfig: HttpClientConfig<out HttpClientEngineConfi
     }
 }
 
-abstract class BaseIosHttpConfig : BaseHttpConfig<DarwinClientEngineConfig>()
+abstract class BaseOkHttpHttpConfig : BaseHttpConfig<OkHttpConfig>()
